70d0e1423829bbb530897d44435dc6fbde1044ca
Mary Melnikova <marymelnikova1988@gmail.com>
2023-10-12 00:19:36 +0300

    Change sample channels

--- src/channel.py
+++ src/channel.py
@@ -8,10 +8,6 @@ api_key: str = os.getenv('API_KEY')
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-def printj(dict_to_print: dict) -> None:
-    """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-
 class Channel:
     """Класс для ютуб-канала"""
 
@@ -19,9 +15,8 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
-
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
+        """Выводит словарь в json-подобном удобном формате с отступами"""
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        printj(channel)
-
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
